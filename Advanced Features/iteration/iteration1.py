e2bcd3f49407be8a665551ddad0ccb9ccb9b3a88
Wasdns <952693358@qq.com>
2017-02-05 17:12:33 +0800

    reorganize the structure
    reorganize the structure

---
+++ Advanced Features/iteration/iteration1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+L = ['it', 'is', 'a', 'list']
+
+for i in range(4) :
+	print(L[i])
+
+T = ('it', 'is', 'a', 'tuple')
+
+for i in range(4) :
+	print(T[i])
+
+dic = {'Chen' : 20, 'Number' : '952693358', 'City' : 'FuZhou'}
+
+for key in dic : 
+	print(key, dic[key])
+
+str1 = 'HeyGirl'
+
+for i in str1 :
+	print(i)
+
+L = ['Chen', 'Mac', 'IPhone', 'Github']
+
+for i, value in enumerate(L) :
+	print(i, value)
+
